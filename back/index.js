ed5dc07575982cfc53cb43c636602aee3abda94d
Meriem-trabelsi <meriem.trabelsi@etudiant-fst.utm.tn>
2025-03-08 00:45:58 +0100

    1

--- back/index.js
+++ back/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const mysql = require("mysql");
+const cors = require('cors');
 const app = express();
 
-app.use(express.json()); // Middleware for JSON parsing
+
+app.use(bodyParser.json());
+const corsOptions = {
+  origin: 'http://localhost:4200/',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+
+// Pool configuration
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'techshop'
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello, Express Backend!");
